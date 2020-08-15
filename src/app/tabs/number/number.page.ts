2775bd3b4d2c2f4a17f9894f14e5ce62dc67a140
Romario <romaef23@gmail.com>
2020-08-15 17:04:05 -0500

    Adding the functionality phone and code
    validations in front and back

--- src/app/tabs/number/number.page.ts
+++ src/app/tabs/number/number.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MbscNumpadDateOptions } from '@mobiscroll/angular';
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 
 
 @Component({
@@ -8,28 +8,100 @@ import { MbscNumpadDateOptions } from '@mobiscroll/angular';
   styleUrls: ['number.page.scss']
 })
 export class NumberPage implements OnInit {
+  togglePhoneCode: boolean = true;
+  confirmPhone: boolean
+  confirmCode: boolean
   dataHeader: any = { 'id': 1, 'name': 'NÚMERO DE CELULAR', 'behind': '' }
   dataSubHeader: any = {
     'title': '',
     'desc': {
       'normal': 'Para comenzar, por favor ingresa',
-      'bold': 'tu número celular'
+      'bold': 'tu número celular',
+      'other': ''
     },
     'img': 'oink.png'
   }
 
+
   number: any = {
     code: '57',
-    phone: '3212546262'
-  };
+    phone: ''
+  }
 
-  constructor() { }
+  code: any = {
+    'protect': 'XXXX',
+    'number': ''
+  }
+
+  constructor(
+    public router: Router
+  ) { }
 
   ngOnInit() {
   }
 
   addNumber(num: number) {
+    // phone number
+    if (this.togglePhoneCode) {
+      if (this.number.phone.length < 10) {
+        this.number.phone += '' + num
+      }
+      if (this.number.phone.length == 10) {
+        this.confirmPhone = true
+      }
+    }
 
+    // code of confirmation
+    if (!this.togglePhoneCode) {
+      if (this.code.number.length < 4) {
+        this.code.number += num
+        this.code.protect = this.code.protect.slice(0, -1)
+      }
+      if (this.code.number.length == 4) {
+        this.confirmCode = true;
+      }
+    }
+  }
+
+  deleteNumber() {
+    // phone number
+    if (this.togglePhoneCode) {
+      if (this.number.phone.length >= 0) {
+        this.number.phone = this.number.phone.slice(0, -1)
+      }
+      if (this.number.phone.length < 10) {
+        this.confirmPhone = false
+      }
+    }
+
+    // code of confirmation
+    if (!this.togglePhoneCode) {
+      this.code.number = ''
+      this.code.protect = 'XXXX'
+      this.confirmCode = false
+      if (this.code.number.length < 4) {
+        this.confirmCode = false
+      }
+    }
+  }
+
+  confirmNumber() {
+    if (this.number.phone.length == 10) {
+      this.togglePhoneCode = false;
+      this.dataSubHeader.desc.normal = 'Digita el código que recibiste vía';
+      this.dataSubHeader.desc.bold = 'mensaje de texto';
+      this.dataSubHeader.desc.bold = 'al ' + this.number.phone;
+    }
+  }
+
+  confirmTheCode() {
+    if (this.code.number.length == 4) {
+      this.router
+        .navigateByUrl('/tabs/tab2', { replaceUrl: true })
+        .then(() => {
+
+        })
+    }
   }
 
 }
